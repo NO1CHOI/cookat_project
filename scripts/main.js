77f9266c9a2f75c4ce976672cbafa6d63b1f0c00
최현아 <kitty05307@naver.com>
2025-08-05 18:58:31 +0900

    css 완성

--- scripts/main.js
+++ scripts/main.js
@@ -35,11 +35,8 @@ const bestHot = new Swiper('#best_hot',{
     spaceBetween:60,
     loop:true,
     navigation:{
-        nextEl:'.swiper-button-prev',
-        prevEl:'.swiper-button-next',
-    },
-    scrollbar: {
-        el: '.swiper-scrollbar',
+        nextEl:'#hot_next',
+        prevEl:'#hot_prev',
     },
 })
 /* 재구매가 많은 */
@@ -48,11 +45,8 @@ const bestRepurchase = new Swiper('#best_repurchase',{
     spaceBetween:60,
     loop:true,
     navigation:{
-        nextEl:'.swiper-button-prev',
-        prevEl:'.swiper-button-next',
-    },
-    scrollbar: {
-        el: '.swiper-scrollbar',
+        nextEl:'#repurchase_next',
+        prevEl:'#repurchase_prev',
     },
 })
 
@@ -62,11 +56,8 @@ const bestSum = new Swiper('#best_sum',{
     spaceBetween:60,
     loop:true,
     navigation:{
-        nextEl:'.swiper-button-prev',
-        prevEl:'.swiper-button-next',
-    },
-    scrollbar: {
-        el: '.swiper-scrollbar',
+        nextEl:'#sum_next',
+        prevEl:'#sum_prev',
     },
 })
 /* 명예의전당 */
@@ -75,11 +66,8 @@ const bestTopranking = new Swiper('#best_topranking',{
     spaceBetween:60,
     loop:true,
     navigation:{
-        nextEl:'.swiper-button-prev',
-        prevEl:'.swiper-button-next',
-    },
-    scrollbar: {
-        el: '.swiper-scrollbar',
+        nextEl:'#topranking_next',
+        prevEl:'#topranking_prev',
     },
 })
 
@@ -98,6 +86,7 @@ const contents = document.querySelectorAll('.sub_item');
 console.log(bestLi,contents)
 
 bestLi.forEach((tab,idx)=>{
+    items[0].classList.add('active');
     tab.addEventListener('click',()=>{
         bestLi.forEach( other => {other.classList.remove('active')})
         tab.classList.add('active');
@@ -109,12 +98,36 @@ bestLi.forEach((tab,idx)=>{
 /* 타임특가 swiper*/
 const timeSaleSwiper = new Swiper('#time_swiper', { 
     slidesPerView:3,
-    spaceBetween:20,
-    autoplay:{delay:5000,},
-    speed:1000,
+    spaceBetween:60,
+    speed:500,
     loop:true,
     navigation:{
-        nextEl:'#timesale_right',
-        prevEl:'#timesale_left',
+        nextEl:'#timesale_left',
+        prevEl:'#timesale_right',
     },
 })
+
+/* 타임특가 시간 설정 */
+function updateTimer() {
+const future = Date.parse("2025/08/30 00:00:00");
+const now = new Date();
+const diff = future - now;
+
+const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+const hours = Math.floor(diff / (1000 * 60 * 60));
+const mins = Math.floor(diff / (1000 * 60));
+const secs = Math.floor(diff / 1000);
+
+const d = days;
+const h = hours - days * 24;
+const m = mins - hours * 60;
+const s = secs - mins * 60;
+document.getElementById("timer")
+.innerHTML =
+'<div>' + d + '<span>Days</span></div>' +
+'<div>' + h + '<span>Hours</span></div>' +
+'<div>' + m + '<span>Minutes</span></div>' +
+'<div>' + s + '<span>Seconds</span></div>';
+}
+
+setInterval(updateTimer, 1000);
